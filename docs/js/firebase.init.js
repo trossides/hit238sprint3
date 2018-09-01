70e99d92af9ab2036be9b484827a3bd0fd1285ec
trossides <theorossides@gmail.com>
2018-09-01 11:15:17 +0930

    page fix

---
+++ docs/js/firebase.init.js
@@ -0,0 +1,37 @@
+angular.module('firebaseConfig', ['firebase'])
+
+.run(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDgC7WbYIVLnAyCql2fXOhwsCERl-HcdqY",
+    authDomain: "authfirebase-40b1e.firebaseapp.com",
+    databaseURL: "https://authfirebase-40b1e.firebaseio.com",
+    storageBucket: "authfirebase-40b1e.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+})
+
+/*
+
+.service("TodoExample", ["$firebaseArray", function($firebaseArray){
+    var ref = firebase.database().ref().child("todos");
+    var items = $firebaseArray(ref);
+    var todos = {
+        items: items,
+        addItem: function(title){
+            items.$add({
+                title: title,
+                finished: false
+            })
+        },
+        setFinished: function(item, newV){
+            item.finished = newV;
+            items.$save(item);
+        }
+    }
+    return todos;
+}])
+
+*/
